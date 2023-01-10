450dbfe287b3383dbfd7cad169d905efe0b74628
Pelidyai <dima-pile@mail.ru>
2023-01-10 22:41:07 +0700

    adapt #2

--- request/updaters.py
+++
@@ -1,6 +0,0 @@
-from jarvis_db.fill.updaters import DataBaseUpdater
-
-
-class WildberriesDBUpdater(DataBaseUpdater):
-    # maybe it can use wildberries providers
-    pass
--- services/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join, abspath
-
-splitted: list[str] = abspath(__file__).split(os.sep)[:-1]
-splitted[0] += os.sep
-rootpath: str = join(*splitted)
-
-data_path: str = join(rootpath, "../data")
-out_path: str = join(rootpath, "out")
