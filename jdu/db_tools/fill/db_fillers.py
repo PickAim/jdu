231a6a6d4d2aefdb8c0ee9b09ed92157c5274ae4
Pelidyai <dima-pile@mail.ru>
2023-07-31 23:53:15 +0700

    Add default warehouse init logic to avoid not existed default warehouse for marketplace

--- jdu/db_tools/fill/db_fillers.py
+++ jdu/db_tools/fill/db_fillers.py
@@ -2,10 +2,12 @@ from abc import ABC
 from abc import abstractmethod
 from typing import Type
 
+from jorm.market.infrastructure import Niche, Marketplace, Warehouse, Address, HandlerType
+from jorm.support.constants import DEFAULT_WAREHOUSE_NAME
+from sqlalchemy.orm import Session
+
 from jdu.db_tools.fill.base import DBFiller
 from jdu.db_tools.fill.initializers import DBFillerInitializer
-from jorm.market.infrastructure import Niche, Marketplace
-from sqlalchemy.orm import Session
 
 
 class __InitializableDBFiller(DBFiller):
@@ -13,11 +15,17 @@ class __InitializableDBFiller(DBFiller):
         super().__init__()
         db_initializer_class().init_db_filler(session, self)
         self.__try_to_init_marketplace_in_db()
+        self.__try_to_init_default_warehouse()
 
     def __try_to_init_marketplace_in_db(self):
         if not self.marketplace_service.exists_with_name(self.marketplace_name):
             self.marketplace_service.create(Marketplace(self.marketplace_name))
         self.marketplace_id = self.marketplace_service.find_by_name(self.marketplace_name)[1]
+
+    def __try_to_init_default_warehouse(self):
+        default_warehouse = Warehouse(DEFAULT_WAREHOUSE_NAME, 123, HandlerType.MARKETPLACE, Address())
+        if self.warehouse_service.find_by_global_id(self.marketplace_id, 123) is None:
+            self.warehouse_service.create_warehouse(default_warehouse, self.marketplace_id)
 
 
 class SimpleDBFiller(__InitializableDBFiller):
