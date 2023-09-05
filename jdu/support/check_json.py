389fd4f00761d137ff0cfe127004a3d2afdaa376
Sawyron <s4pakvv@gmail.com>
2023-09-05 19:44:50 +0700

    replace any function to Any type

--- jdu/support/check_json.py
+++ jdu/support/check_json.py
@@ -1,4 +1,7 @@
-def has_similar(commission_json: dict[str, any]) -> bool:
+from typing import Any
+
+
+def has_similar(commission_json: dict[str, Any]) -> bool:
     niche_list: set[str] = set()
     for niche_name in commission_json:
         niche_list.add(niche_name)
