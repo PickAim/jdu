ad92cc709d0041fa5e21cfed9734ad19db7d62b9
Pelidyai <dima-pile@mail.ru>
2023-09-09 19:53:46 +0700

    Remove redundant things

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,26 +3,20 @@ from jdu.providers.wildberries_providers import (
     WildberriesDataProviderWithoutKeyImpl,
     WildberriesUserMarketDataProviderImpl,
 )
-from tests.basic_db_test import AUTH_KEY
 from tests.initializers.wildberries_initializer import (
     TestWildberriesDataProviderInitializer,
 )
-from tests.providers.wildberries_test_provider import (
-    TestWildberriesDataProviderWithoutKeyImpl,
-)
+
+AUTH_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9' \
+           '.eyJhY2Nlc3NJRCI6IjhiMGZkZWEwLWYxYjgtNDVjOS05NmM5LTdiMmRlNjU2N2Q3ZCJ9' \
+           '.6YAvO_GYeXW3em8WZ5cLynTBKcg8x5pmMmoCkgMY6QI'
 
 
-def create_wb_real_data_provider_without_key() -> WildberriesDataProviderWithoutKey:
+def create_wb_data_provider_without_key() -> WildberriesDataProviderWithoutKey:
     return WildberriesDataProviderWithoutKeyImpl(TestWildberriesDataProviderInitializer)
 
 
-def create_test_wb_data_provider_without_key() -> WildberriesDataProviderWithoutKey:
-    return TestWildberriesDataProviderWithoutKeyImpl(
-        TestWildberriesDataProviderInitializer
-    )
-
-
-def create_real_wb_data_provider_with_key() -> WildberriesUserMarketDataProviderImpl:
+def create_wb_data_provider_with_key() -> WildberriesUserMarketDataProviderImpl:
     return WildberriesUserMarketDataProviderImpl(
         AUTH_KEY, TestWildberriesDataProviderInitializer
     )
