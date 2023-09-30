ecfa2b853cdbc496fd86f0ed4a419376d1b65511
Pelidyai <dima-pile@mail.ru>
2023-09-30 18:00:18 +0700

    JORM propagation. Raise version (v0.1.21)

--- jdu/support/utils.py
+++ jdu/support/utils.py
@@ -11,12 +11,12 @@ def split_to_batches(any_list: list[Any], batch_size: int) -> list[Any]:
 
 def __divide_chunks(any_list: list[Any], chunk_size: int):
     for i in range(0, len(any_list), chunk_size):
-        yield any_list[i : i + chunk_size]
+        yield any_list[i: i + chunk_size]
 
 
 def map_to_dict(
-    key_generator: Callable[[T], V],
-    value_generator: Callable[[T], K],
-    items: Iterable[T],
+        key_generator: Callable[[T], V],
+        value_generator: Callable[[T], K],
+        items: Iterable[T],
 ) -> dict[V, K]:
     return {key_generator(item): value_generator(item) for item in items}
